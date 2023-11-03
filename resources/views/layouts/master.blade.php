033f3cb130259648a5b1e90e000679eb967ffe9d
Eng-MuhammadAbdulrazek <eng.muhammadabdulrazek@gmail.com>
2023-11-03 17:47:54 +0200

    Removed Page Preloader

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="HTML5 Template" />
-    <meta name="description" content="Webmin - Bootstrap 4 & Angular 5 Admin Dashboard Template" />
-    <meta name="author" content="potenzaglobalsolutions.com" />
+    <meta name="keywords" content="School Management System" />
+    <meta name="description" content="School Management System By Medo19" />
+    <meta name="author" content="eng.muhammadabdulrazek@gmail.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     @include('layouts.head')
 </head>
@@ -17,10 +17,6 @@
 
         <!--=================================
  preloader -->
-
-        <div id="pre-loader">
-            <img src="assets/images/pre-loader/loader-01.svg" alt="">
-        </div>
 
         <!--=================================
  preloader -->
