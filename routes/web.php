1dc8a3b69fb80e16f981c0c98b939fa0f842f202
Eng-MuhammadAbdulrazek <eng.muhammadabdulrazek@gmail.com>
2023-11-04 16:41:27 +0200

    Created The Following : - Grades Table - Grades Controller - Grade Model - The Schema & Migrations - Added simple Form for grades adding - Created the route resource in Routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
+
 
 Route::group(
     [
@@ -22,6 +21,8 @@ Route::group(
         Route::get('/', function () {
             return view('dashboard');
         });
+
+        Route::resource('grade', 'GradeController');
 
     });
 
